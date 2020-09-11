5461a36a35510e9044afcc480a2c8038be037929
Marcos Pérez <marcosperez@Marcos-Perez.local>
2020-09-11 12:16:06 +0200

    Added some server logic

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Fist middelware');
-  next();
-});
-
-
-app.use((req, res, next) => {
- res.send("Hello from express!");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fahqwe12',
+      title: 'First serve-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: 'rdtyuio12134',
+      title: 'Second serve-side post',
+      content: 'Hi Im coming from the server too'
+    }
+  ];
+  res.status(200).json({
+    message: 'Success',
+    posts: posts
+  });
 });
 
 module.exports = app;
