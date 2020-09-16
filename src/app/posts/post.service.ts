3ab2b25023fc23bbe32ec7db7e62fe4b12258377
Marcos Pérez <marcosperez@Marcos-Perez.local>
2020-09-16 17:25:39 +0200

    Added backend endpoint for posts

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import {HttpClient} from '@angular/common/http' ;
+import { HttpClient } from '@angular/common/http';
 
 import { Post } from './post.model';
 
@@ -9,14 +9,14 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient ){}
+  constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-    .subscribe((postData) =>{
-      this.posts = postData.posts;
-      this.postsUpdated.next([...this.posts]);
-    });
+    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -24,8 +24,13 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {id: null , title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    const post: Post = { id: null, title: title, content: content };
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((resonseData) => {
+        console.log(resonseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+
   }
 }
